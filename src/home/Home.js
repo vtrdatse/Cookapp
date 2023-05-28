59d289922c4c4e50bafc1c24e309a1756a51b53c
Dung <dungcvse160200@fpt.edu.vn>
2023-05-28 17:07:01 +0700

    homepage

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,17 +1,17 @@
-import React from "react";
-import Splides from "./Splide";
-import Title1 from "./Title1";
-import Title2 from "./Title2";
-import Center from "./Center";
-import "./Home.scss";
+import React from 'react';
+import Splides from './Splide';
+import Title1 from './Title1';
+import Title2 from './Title2';
+import Center from './Center';
+import './Home.css';
 
 export default function Home() {
-    return (
-        <>
-            <Splides />
-            <Title1 />
-            <Center />
-            <Title2 />
-        </>
-    );
+	return (
+		<div className="home">
+			<Splides />
+			<Title1 />
+			<Center />
+			<Title2 />
+		</div>
+	);
 }
