bdaefa63c6311f62c6bcc667ac75bc7c9e559c86
Dung <dungcvse160200@fpt.edu.vn>
2023-05-29 13:31:31 +0700

    Revert "homepage"
    This reverts commit 59d289922c4c4e50bafc1c24e309a1756a51b53c.

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import Splides from './Splide';
-import Title1 from './Title1';
-import Title2 from './Title2';
-import Center from './Center';
-import './Home.css';
+import React from "react";
+import Splides from "./Splide";
+import Title1 from "./Title1";
+import Title2 from "./Title2";
+import Center from "./Center";
+import "./Home.scss";
 
 export default function Home() {
-	return (
-		<div className="home">
-			<Splides />
-			<Title1 />
-			<Center />
-			<Title2 />
-		</div>
-	);
+    return (
+        <>
+            <Splides />
+            <Title1 />
+            <Center />
+            <Title2 />
+        </>
+    );
 }
