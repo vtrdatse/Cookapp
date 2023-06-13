443ab250394f91ed52d3694daacbc885457c0ee5
DK <datvotrongmod68@gmail.com>
2023-06-13 16:41:45 +0700

    Go to Recipe

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import RecipesListPage from './pages/RecipesListPage';
 import 'swiper/css';
+// import Home from './home/Home';
+
 
 function App() {
 	return <RecipesListPage />;
