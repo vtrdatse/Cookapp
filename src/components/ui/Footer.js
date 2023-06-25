5d3265589e9859cb61ebc4fb696871ce9040c266
DK <datvotrongmod68@gmail.com>
2023-06-25 11:33:43 +0700

    Footer

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="footer-container">
-      <span>@2023 Developed By VTD</span>
+      <span>@2023 Developed By CookApp</span>
     </div>
   );
 };
